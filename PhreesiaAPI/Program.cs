be354d1db4002c832aca5a8b934bc349962a2c82
Alberto Hirota <alberto.hirota@gmail.com>
2022-11-28 20:09:10 -0500

    Add project files.

---
+++ PhreesiaAPI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+using Newtonsoft.Json;
+
+
+namespace PhreesiaAPI 
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
