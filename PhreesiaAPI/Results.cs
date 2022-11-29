6587296dcc7e8b4cad630885e9b833eeb78f458a
Alberto Hirota <alberto.hirota@gmail.com>
2022-11-29 18:00:18 -0500

    Test 3.2 done

---
+++ PhreesiaAPI/Results.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace PhreesiaAPI
+{
+    class Results
+    {
+        [JsonProperty("numFound")]
+        public int? TotalBooks { get; set; }
+        public int? start { get; set; }
+        public bool? numFoundExact { get; set; }
+        [JsonProperty("docs")]
+        public List<BookList>? Books { get; set; }
+        public int? num_found { get; set; }
+        public string? q { get; set; }
+        public object? offset { get; set; }
+    }
+}
